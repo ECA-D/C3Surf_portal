1da58ea09f0c184c370b2e96e71a64249ffb887b
evdbesselaar <besselaar@knmi.nl>
2018-07-25 13:46:43 +0000

    Monthly E-OBS update

--- include/allnews.php
+++ include/allnews.php
@@ -65,6 +65,10 @@ session_start();
 		      <td width="60%" style="color:#FFFFFF;"><b>Item</b></td>
 		    </tr>
 		    <tr>
+		      <td>26 July 2018</td>
+	              <td>Monthly <a href="<?php echo $webhost;?>/dataaccess/access_eobs_months.php">E-OBS</a> update released</td>
+		    </tr>    
+		    <tr>
 		      <td>26 June 2018</td>
 	              <td>Monthly <a href="<?php echo $webhost;?>/dataaccess/access_eobs_months.php">E-OBS</a> update released</td>
 		    </tr>    
