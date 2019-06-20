046a804c962ed86a2a1940d56db38b95512936c4
evdbesselaar <besselaar@knmi.nl>
2019-06-20 11:09:56 +0000

    typo news items

--- include/menu_portal.php
+++ include/menu_portal.php
@@ -46,7 +46,8 @@ session_start();
 		    </div>
 
 		    <div class="views-row views-row-1 views-row-odd views-row-first line-separator">
-       		      <div class="field-content"><span content="2018-04-24T00:00:00+02:00" datatype="xsd:dateTime" property="dc:date" class="date-display-single">29 May 2019</span>
+		      <div class="views-field views-field-field-date">
+			<div class="field-content"><span content="2018-04-23T00:00:00+02:00" datatype="xsd:dateTime" property="dc:date" class="date-display-single">29 May 2019</span>
 			</div>
 		      </div>  
 		      <div class="views-field views-field-title"><span class="field-content"><a href="<?php echo $webhost;?>/dataaccess/access_eobs_indices.php">Indices based on E-OBSv19.0e</a> released</span>
