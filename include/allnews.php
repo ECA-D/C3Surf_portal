6b2af12ba1df90f58184529acb5901b34fef9798
evdbesselaar <besselaar@knmi.nl>
2019-09-12 08:10:00 +0000

    Monthly E-OBS update

--- include/allnews.php
+++ include/allnews.php
@@ -65,6 +65,10 @@ session_start();
 		      <td width="60%" style="color:#FFFFFF;"><b>Item</b></td>
 		    </tr>
 		    <tr>
+		      <td>12 September 2019</td>
+	              <td>Monthly <a href="<?php echo $webhost;?>/dataaccess/access_eobs_months.php">E-OBS</a> update released</td>
+		    </tr> 
+		    <tr>
 		      <td>26 August 2019</td>
 	              <td><a href="../stateoftheclimate/july2019.php">State-of-the-climate for July 2019</a> released</td>
 		    </tr>		  
