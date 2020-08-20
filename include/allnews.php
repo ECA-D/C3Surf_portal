db77da3b1a022b4eb1aee2ef5bf61f7e97020daf
evdbesselaar <besselaar@knmi.nl>
2020-08-20 13:16:01 +0000

    Monthly E-OBS news item

--- include/allnews.php
+++ include/allnews.php
@@ -65,6 +65,10 @@ session_start();
 		      <td width="60%" style="color:#FFFFFF;"><b>Item</b></td>
 		    </tr>
 		    <tr>
+		      <td>13 August 2020</td>
+	              <td>Monthly <a href="<?php echo $webhost;?>/dataaccess/access_eobs_months.php">E-OBS</a> update released</td>
+		    </tr> 
+		    <tr>
 		      <td>29 July 2020</td>
 	              <td><a href="../stateoftheclimate/june2020.php">State-of-the-climate for June 2020</a> released</td>
 		    </tr>		  
