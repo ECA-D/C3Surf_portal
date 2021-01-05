5fecadeaf0d54244bbc544e9081977c60d3c965b
evdbesselaar <besselaar@knmi.nl>
2021-01-05 08:24:41 +0000

    Monthly E-OBS update (nov 2020)

--- include/allnews.php
+++ include/allnews.php
@@ -65,6 +65,10 @@ session_start();
 		      <td width="60%" style="color:#FFFFFF;"><b>Item</b></td>
 		    </tr>
 		    <tr>
+		      <td>5 January 2021</td>
+	              <td>Monthly <a href="<?php echo $webhost;?>/dataaccess/access_eobs_months.php">E-OBS</a> update released (for November 2020)</td>
+		    </tr> 
+		    <tr>
 		      <td>3 December 2020</td>
 	              <td><a href="<?php echo $webhost;?>/dataaccess/access_eobs.php">E-OBS v22.0e</a> released</td>
 		    </tr>		  
