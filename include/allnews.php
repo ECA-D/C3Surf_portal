4d28ee39651a0d612d270f0278c42fd9a000bbd7
evdbesselaar <besselaar@knmi.nl>
2021-04-26 11:22:37 +0000

    monthly eobs

--- include/allnews.php
+++ include/allnews.php
@@ -74,6 +74,10 @@ session_start();
 	              <td><a href="https://climate.copernicus.eu/ESOTC/2020">Annual State-of-the-climate 2020</a> released</td>
 		    </tr>		  
 		    <tr>
+		      <td>26 April 2021</td>
+	              <td>Monthly <a href="<?php echo $webhost;?>/dataaccess/access_eobs_months.php">E-OBS</a> update released</td>
+		    </tr> 
+		    <tr>
 		      <td>30 March 2021</td>
 	              <td>E-OBS v23.0e has been replaced by <a href="<?php echo $webhost;?>/dataaccess/access_eobs.php">E-OBS v23.1e</a>. Only de temperature files for Jun-Dec 2020 have been affected by this in Northern France.</td>
 		    </tr>
