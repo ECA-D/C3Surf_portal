8632b588d0ddf15ae893ec6f53c4847383e5fec9
evdbesselaar <besselaar@knmi.nl>
2021-08-16 11:38:39 +0000

    Wind files 03/2020-12/2020 for Cornel

---
+++ dataaccess/monthly_files_wind.php
@@ -0,0 +1,247 @@
+<?php
+error_reporting(E_ALL  & ~E_NOTICE);
+session_start();
+?>
+<!DOCTYPE html>
+<html style="" class="js js canvas no-touch rgba opacity cssanimations csstransforms csstransforms3d csstransitions svg inlinesvg WebfontLoader-processed bootstrap-anchors-processed wf-opensans-n4-active wf-opensans-n7-active wf-active" dir="ltr" prefix="content: https://purl.org/rss/1.0/modules/content/ dc: https://purl.org/dc/terms/ foaf: https://xmlns.com/foaf/0.1/ og: https://ogp.me/ns# rdfs: https://www.w3.org/2000/01/rdf-schema# sioc: https://rdfs.org/sioc/ns# sioct: https://rdfs.org/sioc/types# skos: https://www.w3.org/2004/02/skos/core# xsd: https://www.w3.org/2001/XMLSchema#" lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="shortcut icon" href="../theme/favicon_0.png" type="image/png">
+    <link rel="profile" href="https://www.w3.org/1999/xhtml/vocab">
+    <meta name="generator" content="Drupal 7 (https://drupal.org)">
+    <link rel="canonical" href="https://climate.copernicus.eu/events">
+    <link rel="shortlink" href="https://climate.copernicus.eu/events">
+    <title>Data</title>
+    
+    <script src="https://www.w3schools.com/lib/w3.js"></script>
+ 
+
+  <?php include('../include/styles.php'); ?>
+  
+  </head>
+
+
+  <body class="html not-front not-logged-in no-sidebars page-events ff ff43 win desktop">
+   <?php include('../include/header.php'); ?>  
+
+    <!-- Title -->
+    <div id="globalpageheaderwrapper" class="wrapper clearfix ">
+      <div id="globalpageheadercontainer" class="container">
+	<div id="globalpageheader">
+          <div class="row">
+	    <div class="region region-static-page-title col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
+	      <div class="row-fluid">
+		<div id="block-delta-blocks-page-title" class="block block-delta-blocks col-xs-12 col-sm-12 col-md-12 col-lg-12 clearfix">
+		  <h1 id="page-title" class="title">Old dat files</h1>
+		</div> <!-- /.block -->
+	      </div>
+	    </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div id="mainwrapper" class="wrapper clearfix ">
+      <div id="maincontainer" class="container">
+        <div id="main">
+          <div class="row-fluid">
+            <div class="region region-content  col-xs-12 col-sm-12 col-md-8 col-lg-8">
+	      <div class="row-fluid">
+		
+		<!-- Breadcrum -->
+		<h2 class="element-invisible">You are here</h2><div class="breadcrumb"><span class="inline odd first"><a href="../../surfobs.php">Home</a></span><span class="delimiter">&raquo;</span> <span class="inline even last"><a href="monthly_files_wind.php">Old data files</a></span></div>
+
+		<!-- Main part -->
+		<div id="block-system-main" class="block block-system col-xs-12 col-sm-12 col-md-12 col-lg-12 clearfix">
+
+
+ 
+                    <table  border="0" style="padding: 8px; background: #f4f4f4; border-bottom: 1px solid #999; box-shadow: 0px 5px 5px #666; margin-bottom: 20px;" width="100%">
+                  <tbody>
+
+                    <tr bgcolor="#941333" height="40px">
+                      <td width="25%" style="color:#FFFFFF;"><b>December 2020</b></td>
+                      <td width="25%" style="color:#FFFFFF;"><b>Ensemble median</b></td>
+                      <td width="25%" style="color:#FFFFFF;"><b>Ensemble spread</b></td>
+                    </tr>
+                    <tr>
+                      <td>0.1 degree regular grid</td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_mean_0.1deg_reg_2020_12.nc">FG</a></td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_spread_0.1deg_reg_2020_12.nc">FG</a></td>
+                    </tr>
+                     <tr>
+                      <td>0.25 degree regular grid</td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_mean_0.25deg_reg_2020_12.nc">FG</a></td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_spread_0.25deg_reg_2020_12.nc">FG</a>
+</td>
+                    </tr>
+
+                    <tr bgcolor="#941333" height="40px">
+                      <td width="25%" style="color:#FFFFFF;"><b>November 2020</b></td>
+                      <td width="25%" style="color:#FFFFFF;"><b>Ensemble median</b></td>
+                      <td width="25%" style="color:#FFFFFF;"><b>Ensemble spread</b></td>
+                    </tr>
+                    <tr>
+                      <td>0.1 degree regular grid</td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_mean_0.1deg_reg_2020_11.nc">FG</a></td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_spread_0.1deg_reg_2020_11.nc">FG</a></td>
+                    </tr>
+                     <tr>
+                      <td>0.25 degree regular grid</td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_mean_0.25deg_reg_2020_11.nc">FG</a></td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_spread_0.25deg_reg_2020_11.nc">FG</a>
+</td>
+                    </tr>
+
+                    <tr bgcolor="#941333" height="40px">
+                      <td width="25%" style="color:#FFFFFF;"><b>October 2020</b></td>
+                      <td width="25%" style="color:#FFFFFF;"><b>Ensemble median</b></td>
+                      <td width="25%" style="color:#FFFFFF;"><b>Ensemble spread</b></td>
+                    </tr>
+                    <tr>
+                      <td>0.1 degree regular grid</td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_mean_0.1deg_reg_2020_10.nc">FG</a></td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_spread_0.1deg_reg_2020_10.nc">FG</a></td>
+                    </tr>
+                     <tr>
+                      <td>0.25 degree regular grid</td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_mean_0.25deg_reg_2020_10.nc">FG</a></td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_spread_0.25deg_reg_2020_10.nc">FG</a>
+</td>
+                    </tr>
+
+                    <tr bgcolor="#941333" height="40px">
+                      <td width="25%" style="color:#FFFFFF;"><b>September 2020</b></td>
+                      <td width="25%" style="color:#FFFFFF;"><b>Ensemble median</b></td>
+                      <td width="25%" style="color:#FFFFFF;"><b>Ensemble spread</b></td>
+                    </tr>
+                    <tr>
+                      <td>0.1 degree regular grid</td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_mean_0.1deg_reg_2020_09.nc">FG</a></td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_spread_0.1deg_reg_2020_09.nc">FG</a></td>
+                    </tr>
+                     <tr>
+                      <td>0.25 degree regular grid</td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_mean_0.25deg_reg_2020_09.nc">FG</a></td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_spread_0.25deg_reg_2020_09.nc">FG</a>
+</td>
+                    </tr>
+
+                    <tr bgcolor="#941333" height="40px">
+                      <td width="25%" style="color:#FFFFFF;"><b>August 2020</b></td>
+                      <td width="25%" style="color:#FFFFFF;"><b>Ensemble median</b></td>
+                      <td width="25%" style="color:#FFFFFF;"><b>Ensemble spread</b></td>
+                    </tr>
+                    <tr>
+                      <td>0.1 degree regular grid</td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_mean_0.1deg_reg_2020_08.nc">FG</a></td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_spread_0.1deg_reg_2020_08.nc">FG</a></td>
+                    </tr>
+                     <tr>
+                      <td>0.25 degree regular grid</td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_mean_0.25deg_reg_2020_08.nc">FG</a></td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_spread_0.25deg_reg_2020_08.nc">FG</a>
+</td>
+                    </tr>
+
+                    <tr bgcolor="#941333" height="40px">
+                      <td width="25%" style="color:#FFFFFF;"><b>July 2020</b></td>
+                      <td width="25%" style="color:#FFFFFF;"><b>Ensemble median</b></td>
+                      <td width="25%" style="color:#FFFFFF;"><b>Ensemble spread</b></td>
+                    </tr>
+                    <tr>
+                      <td>0.1 degree regular grid</td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_mean_0.1deg_reg_2020_07.nc">FG</a></td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_spread_0.1deg_reg_2020_07.nc">FG</a></td>
+                    </tr>
+                     <tr>
+                      <td>0.25 degree regular grid</td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_mean_0.25deg_reg_2020_07.nc">FG</a></td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_spread_0.25deg_reg_2020_07.nc">FG</a>
+</td>
+                    </tr>
+
+                    <tr bgcolor="#941333" height="40px">
+                      <td width="25%" style="color:#FFFFFF;"><b>June 2020</b></td>
+                      <td width="25%" style="color:#FFFFFF;"><b>Ensemble median</b></td>
+                      <td width="25%" style="color:#FFFFFF;"><b>Ensemble spread</b></td>
+                    </tr>
+                    <tr>
+                      <td>0.1 degree regular grid</td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_mean_0.1deg_reg_2020_06.nc">FG</a></td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_spread_0.1deg_reg_2020_06.nc">FG</a></td>
+                    </tr>
+                     <tr>
+                      <td>0.25 degree regular grid</td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_mean_0.25deg_reg_2020_06.nc">FG</a></td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_spread_0.25deg_reg_2020_06.nc">FG</a>
+</td>
+                    </tr>
+
+                    <tr bgcolor="#941333" height="40px">
+                      <td width="25%" style="color:#FFFFFF;"><b>May 2020</b></td>
+                      <td width="25%" style="color:#FFFFFF;"><b>Ensemble median</b></td>
+                      <td width="25%" style="color:#FFFFFF;"><b>Ensemble spread</b></td>
+                    </tr>
+                    <tr>
+                      <td>0.1 degree regular grid</td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_mean_0.1deg_reg_2020_05.nc">FG</a></td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_spread_0.1deg_reg_2020_05.nc">FG</a></td>
+                    </tr>
+                     <tr>
+                      <td>0.25 degree regular grid</td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_mean_0.25deg_reg_2020_05.nc">FG</a></td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_spread_0.25deg_reg_2020_05.nc">FG</a>
+</td>
+                    </tr>
+                    <tr bgcolor="#941333" height="40px">
+                      <td width="25%" style="color:#FFFFFF;"><b>April 2020</b></td>
+                      <td width="25%" style="color:#FFFFFF;"><b>Ensemble median</b></td>
+                      <td width="25%" style="color:#FFFFFF;"><b>Ensemble spread</b></td>
+                    </tr>
+                    <tr>
+                      <td>0.1 degree regular grid</td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_mean_0.1deg_reg_2020_04.nc">FG</a></td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_spread_0.1deg_reg_2020_04.nc">FG</a></td>
+                    </tr>
+                     <tr>
+                      <td>0.25 degree regular grid</td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_mean_0.25deg_reg_2020_04.nc">FG</a></td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_spread_0.25deg_reg_2020_04.nc">FG</a>
+</td>
+                    </tr>
+
+                    <tr bgcolor="#941333" height="40px">
+                      <td width="25%" style="color:#FFFFFF;"><b>March 2020</b></td>
+                      <td width="25%" style="color:#FFFFFF;"><b>Ensemble median</b></td>
+                      <td width="25%" style="color:#FFFFFF;"><b>Ensemble spread</b></td>
+                    </tr>
+                    <tr>
+                      <td>0.1 degree regular grid</td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_mean_0.1deg_reg_2020_03.nc">FG</a></td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_spread_0.1deg_reg_2020_03.nc">FG</a></td>
+                    </tr>
+                     <tr>
+                      <td>0.25 degree regular grid</td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_mean_0.25deg_reg_2020_03.nc">FG</a></td>
+                      <td> <a href="https://knmi-ecad-assets-prd.s3.amazonaws.com/ensembles/data/months/ens/fg_ens_spread_0.25deg_reg_2020_03.nc">FG</a>
+</td>
+                    </tr>
+		  
+		</div>  
+	      </div>
+	    </div> <!-- /.block -->
+	  </div>
+	</div>
+	<!-- menu_portal.php -->
+	<?php include('../../../../include/menu_portal.php'); ?>
+      </div>	
+    </div>
+    <?php include('../../../../include/footer.php'); ?>
+    <?php include('../../../../include/mobile_menu.php'); ?>
+  </body>
+</html>
