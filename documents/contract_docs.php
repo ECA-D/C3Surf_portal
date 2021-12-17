b39a95ffbff270c867205a0dfa6990e000d8feb6
evdbesselaar <besselaar@knmi.nl>
2021-12-17 09:35:48 +0000

    contract document page

---
+++ documents/contract_docs.php
@@ -0,0 +1,122 @@
+
+<?php
+error_reporting(E_ALL  & ~E_NOTICE);
+session_start();
+?>
+<!DOCTYPE html>
+<html style="" class="js js canvas no-touch rgba opacity cssanimations csstransforms csstransforms3d csstransitions svg inlinesvg WebfontLoader-processed bootstrap-anchors-processed wf-opensans-n4-active wf-opensans-n7-active wf-active" dir="ltr" prefix="content: https://purl.org/rss/1.0/modules/content/ dc: https://purl.org/dc/terms/ foaf: https://xmlns.com/foaf/0.1/ og: https://ogp.me/ns# rdfs: https://www.w3.org/2000/01/rdf-schema# sioc: https://rdfs.org/sioc/ns# sioct: https://rdfs.org/sioc/types# skos: https://www.w3.org/2004/02/skos/core# xsd: https://www.w3.org/2001/XMLSchema#" lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="shortcut icon" href="../theme/favicon_0.png" type="image/png">
+    <link rel="profile" href="https://www.w3.org/1999/xhtml/vocab">
+    <meta name="generator" content="Drupal 7 (https://drupal.org)">
+    <link rel="canonical" href="https://climate.copernicus.eu/events">
+    <link rel="shortlink" href="https://climate.copernicus.eu/events">
+    <title>Contract documents</title>
+    
+    <script src="https://www.w3schools.com/lib/w3.js"></script>
+ 
+
+  <?php include('../include/styles.php'); ?>
+  
+  </head>
+
+
+  <body class="html not-front not-logged-in no-sidebars page-events ff ff43 win desktop">
+   <?php include('../include/header.php'); ?>  
+
+    <!-- Title -->
+    <div id="globalpageheaderwrapper" class="wrapper clearfix ">
+      <div id="globalpageheadercontainer" class="container">
+	<div id="globalpageheader">
+          <div class="row">
+	    <div class="region region-static-page-title col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
+	      <div class="row-fluid">
+		<div id="block-delta-blocks-page-title" class="block block-delta-blocks col-xs-12 col-sm-12 col-md-12 col-lg-12 clearfix">
+		  <h1 id="page-title" class="title">Contract documents</h1>
+		</div> <!-- /.block -->
+	      </div>
+	    </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div id="mainwrapper" class="wrapper clearfix ">
+      <div id="maincontainer" class="container">
+        <div id="main">
+          <div class="row-fluid">
+            <div class="region region-content  col-xs-12 col-sm-12 col-md-8 col-lg-8">
+	      <div class="row-fluid">
+		
+		<!-- Breadcrum -->
+		<h2 class="element-invisible">You are here</h2><div class="breadcrumb"><span class="inline odd first"><a href="../surfobs.php">Home</a></span><span class="delimiter">&raquo;</span> <span class="inline even last"><a href="contract_docs.php">Contract documents</a></span></div>
+
+		<!-- Main part -->
+		<div id="block-system-main" class="block block-system col-xs-12 col-sm-12 col-md-12 col-lg-12 clearfix">
+		  Documentation and presentations from contract meetings.<br><br>
+		  
+		<table  border="0" style="padding: 8px; background: #f4f4f4; border-bottom: 1px solid #999; box-shadow: 0px 5px 5px #666; margin-bottom: 20px;" width="100%">
+		  <tbody>
+                   <tr bgcolor="#941333" height="40px">
+                      <td width="100%" style="color:#FFFFFF;"><b>Kickoff 13 December 2021</b></td>
+                    </tr>
+ 		    <tr>
+		      <td><a href="kickoff_c3surf2/Minutes_kick-off_C3S2_311_Lot3_dec21_v03.pdf">Agenda and minutes of the kick-off meeting</a>
+		      </td>
+		    </tr>
+ 		    <tr>
+		      <td><a href="kickoff_c3surf2/C3S2_311_Lot3_KickOff_AngelikiTsapatsari.pdf">Presentation of Angeliki Tsapatsari</a>
+		      </td>
+		    </tr>
+ 		    <tr>
+		      <td><a href="kickoff_c3surf2/C3S2_311_Lot3_KickOff_HansHersbach.pdf">Presentation of Hans Hersbach</a>
+		      </td>
+		    </tr>
+ 		    <tr>
+		      <td><a href="kickoff_c3surf2/C3S2_311_Lot3_KickOff_MeteoSwiss.pdf">Presentation of MeteoSwiss</a>
+		      </td>
+		    </tr>
+ 		    <tr>
+		      <td><a href="kickoff_c3surf2/C3S2_311_Lot3_KickOff_ZAMG.pdf">Presentation of ZAMG</a>
+		      </td>
+		    </tr>
+ 		    <tr>
+		      <td><a href="kickoff_c3surf2/C3S2_311_Lot3_KickOff_DWD.pdf">Presentation of DWD</a>
+		      </td>
+		    </tr>
+ 		    <tr>
+		      <td><a href="kickoff_c3surf2/C3S2_311_Lot3_KickOff_GCRI.pdf">Presentation of GCRI</a>
+		      </td>
+		    </tr>
+ 		    <tr>
+		      <td><a href="kickoff_c3surf2/C3S2_311_Lot3_KickOff_MetNorway.pdf">Presentation of MetNorway</a>
+		      </td>
+		    </tr>
+ 		    <tr>
+		      <td><a href="kickoff_c3surf2/C3S2_311_Lot3_KickOff_KNMI.pdf">Presentation of KNMI</a>
+		      </td>
+		    </tr>
+
+		  <tbody>
+		</table>
+		
+		</div>	  
+	      </div>
+	      
+	    </div> <!-- /.block -->
+	  </div>
+	</div>
+	<!-- menu_portal.php -->
+       <?php include('../include/menu_portal.php'); ?>
+     </div>	
+    </div>
+    <?php include('../include/footer.php'); ?>
+    <?php include('../include/mobile_menu.php'); ?>
+   
+  </body>
+</html>
