f968cf0da5cd1b5acfea3d430be50815af84ccc3
besselaar <besselaar@knmi.nl>
2024-06-27 11:00:27 +0200

    Monthly qq files and monthly E-OBS indices released

--- include/allnews.php
+++ include/allnews.php
@@ -66,6 +66,11 @@ session_start();
 		    </tr>
 
 		    <tr>
+		      <td>27 June 2024</td>
+	              <td>Monthly <a href="<?php echo $webhost;?>/dataaccess/access_eobs_months.php">E-OBS</a> updates for global radiation and monthly <a href="<?php echo $webhost;?>/dataaccess/access_eobs_indices_months.php">E-OBS indices</a> updates released</td>
+		    </tr>
+
+		    <tr>
 		      <td>25 June 2024</td>
 	              <td>Monthly <a href="<?php echo $webhost;?>/dataaccess/access_eobs_months.php">E-OBS</a> updates (except wind speed and radiation) released</td>
 		    </tr>
--- include/menu_portal.php
+++ include/menu_portal.php
@@ -36,7 +36,16 @@ session_start();
 	  	  <div class="view-content">
 		  <!-- Start news items -->
 
-                    <div class="views-row views-row-1 views-row-odd views-row-first line-separator">
+                      <div class="views-row views-row-1 views-row-odd views-row-first line-separator">
+                      <div class="views-field views-field-field-date">
+                        <div class="field-content"><span content="2018-04-24T00:00:00+02:00" datatype="xsd:dateTime" property="dc:date" class="date-display-single">27 June 2024</span>
+                        </div>
+                      </div>
+                    <div class="views-field views-field-title"><span class="field-content"> Monthly <a href="<?php echo $webhost;?>/dataaccess/access_eobs_months.php">E-OBS</a> updates for global radiation and monthly <a href="<?php echo $webhost;?>/dataaccess/access_eobs_indices_months.php">E-OBS indices</a> updates released</span>
+		    </div>
+                </div>
+
+                      <div class="views-row views-row-1 views-row-odd views-row-first line-separator">
                       <div class="views-field views-field-field-date">
                         <div class="field-content"><span content="2018-04-24T00:00:00+02:00" datatype="xsd:dateTime" property="dc:date" class="date-display-single">25 June 2024</span>
                         </div>
