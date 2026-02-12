88b4b51db19f17181d10a30a9a885bdece08fdb9
Tim Vlemmix / KNMI <vlemmix@pc200040.dynamic.knmi.nl>
2026-02-12 17:11:57 +0000

    Monthly EOBS update

--- include/allnews.php
+++ include/allnews.php
@@ -64,7 +64,13 @@ session_start();
 		      <td width="40%" style="color:#FFFFFF;"><b>Date</b></td>
 		      <td width="60%" style="color:#FFFFFF;"><b>Item</b></td>
 		    </tr>
-		    
+
+		    <tr>
+		      <td>12 February 2026</td>
+		      <td>Monthly <a href="<?php echo $webhost;?>/dataaccess/access_eobs_months.php">E-OBS</a> updates (except global radiation) and monthly <a href="<?php echo $webhost;?>/dataaccess/access_eobs_indices_months.php">E-OBS indices</a> released</td>
+		      </td>
+                    </tr>
+
 		    <tr>
 		      <td>22 January 2026</td>
 		      <td>Monthly <a href="<?php echo $webhost;?>/dataaccess/access_eobs_months.php">E-OBS</a> updates (except global radiation) and monthly <a href="<?php echo $webhost;?>/dataaccess/access_eobs_indices_months.php">E-OBS indices</a> released</td>
