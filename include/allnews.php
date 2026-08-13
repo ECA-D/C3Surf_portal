b77cfeb27ac6704f9bfba4846c0694a30145606b
besselaar <besselaar@knmi.nl>
2026-08-13 15:10:31 +0200

    Monthly E-OBS update

--- include/allnews.php
+++ include/allnews.php
@@ -66,6 +66,12 @@ session_start();
 		    </tr>
 		    
                     <tr>
+		      <td>13 August 2026</td>
+		      <td>Monthly <a href="<?php echo $webhost;?>/dataaccess/access_eobs_months.php">E-OBS</a> updates released</td>
+		      </td>
+                    </tr>
+
+                    <tr>
 		      <td>15 July 2026</td>
 		      <td>Monthly <a href="<?php echo $webhost;?>/dataaccess/access_eobs_months.php">E-OBS</a> updates released</td>
 		      </td>
